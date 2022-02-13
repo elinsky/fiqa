19a05de236a3447106c3aedad4c0955342ee37cc
Brian Elinsky <be2112@gmail.com>
2022-02-13 09:33:11 -0600

    Add data and model configs to config file

--- configs/config.py
+++ configs/config.py
@@ -1,8 +1,14 @@
 """Model config in json format"""
 
 CONFIG = {
+    'data': {
+        'path': 'data'
+    },
     'train': {
         'batch_size': 32,
         'epochs': 10
+    },
+    'model': {
+        'vocab_size': 3000
     }
 }
