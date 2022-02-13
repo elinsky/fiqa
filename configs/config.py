717438e69689f4d11359e89bbbab72d7c90d0ac8
Brian Elinsky <be2112@gmail.com>
2022-02-13 09:40:07 -0600

    Add num class constants to config file

--- configs/config.py
+++ configs/config.py
@@ -2,7 +2,12 @@
 
 CONFIG = {
     'data': {
-        'path': 'data'
+        'path': 'data',
+        'n_level_1_classes': 4,
+        'n_level_2_corporate': 12,
+        'n_level_2_economy': 2,
+        'n_level_2_market': 4,
+        'n_level_2_stock': 9
     },
     'train': {
         'batch_size': 32,
